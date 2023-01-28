2aa8735e60396d95f15b1e005b64cda3b37349e6
Nícolas <nicolasscorzelli@gmail.com>
2023-01-28 01:01:29 -0300

    feat: create .env with right pattern

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,17 +1,29 @@
 import { Sequelize } from 'sequelize'
-import * as dotenv from 'dotenv'
+import dotenv from 'dotenv'
 dotenv.config()
+const ENV = process.env
+const env : any  = {
 
-const sequelize = new Sequelize(
-    'challengedb',
-    'root',
-    'root',
-    {
-        dialect: 'mysql',
-        host: 'localhost',
-        port: 3306,
+    DB_HOST : ENV.DB_HOST,
+    DB_DIALECT : ENV.DB_DIALECT,
+    DB_PORT : ENV.DB_PORT,
+    DB_USERNAME : ENV.DB_USERNAME,
+    DB_PASSWORD : ENV.DB_PASSWORD, 
+    DB_DATABASE : ENV.DB_DATABASE,
+
+}
+
+Object.entries(env).forEach(([key, value]) => {
+    if( value === 'true' || value === 'false') {
+        env[key] = JSON.parse(value.toLowerCase())
     }
-);
+})
+
+const sequelize = new Sequelize( env.DB_DATABASE, env.DB_USERNAME, env.DB_PASSWORD, {
+    host: env.DB_HOST,
+    dialect: env.DB_DIALECT,
+    port: env.DB_PORT,
+})
 
 
-export default sequelize;
+export default sequelize
