9b071bbefe34429b9a0fe1292703ba5c5224f6fe
Nícolas <nicolasscorzelli@gmail.com>
2023-01-30 20:34:40 -0300

    refactor: change PostModel

--- src/modules/models/PostModel.ts
+++ src/modules/models/PostModel.ts
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize'
-import { getAsync, redis, setAsync } from '../../config/config'
+import { getAsync, redis, setAsync } from '../../config/redis'
 import database from '../../shared/database/config'
 import { badRequest, notFound } from '../../shared/errors/helper/http-helper'
 import { MissingParamError } from '../../shared/errors/missing-param-error'
