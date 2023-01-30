71f13ce5567f0b79123bd60adfcf91bd8bcf7d9f
Nícolas <nicolasscorzelli@gmail.com>
2023-01-30 20:48:15 -0300

    fix: valid all routes

--- src/shared/http/Routes/index.ts
+++ src/shared/http/Routes/index.ts
@@ -6,13 +6,13 @@ const routes = Router()
 routes.post('/post', CreatePostService.create)
 routes.post('/comment', CreateCommentService.create)
 
-// routes.get('/posts', CreatePostService.getPosts)
-// routes.get('/post/:id', CreatePostService.getPostById)
+routes.get('/posts', CreatePostService.getPosts)
+routes.get('/post/:id', CreatePostService.getPostById)
 
-// routes.patch('/post/:id', CreatePostService.update)
+routes.patch('/post/:id', CreatePostService.update)
 
-// routes.delete('/post/:id', CreatePostService.delete)
+routes.delete('/post/:id', CreatePostService.delete)
 
-// routes.delete('/comment/:id', CreateCommentService.deleteComment)
+routes.delete('/comment/:id', CreateCommentService.deleteComment)
 
 export default routes
