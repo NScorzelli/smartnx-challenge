861bf31a2126c956a7497fc900238b8b7a09fa56
Nícolas <nicolasscorzelli@gmail.com>
2023-01-30 21:48:58 -0300

    refactor: change midleware return

--- src/shared/http/middlewares/validateRoute.ts
+++ src/shared/http/middlewares/validateRoute.ts
@@ -2,40 +2,40 @@ import { type NextFunction, type Request, type Response } from 'express'
 import { badRequest } from '../../../shared/errors/helper/http-helper'
 import { MissingParamError } from '../../../shared/errors/missing-param-error'
 
-const validateRoute = (request: Request, response: Response, next: NextFunction): void => {
-  const { method, path } = request
-  const { body, params } = request
+const validateRoute = (req: Request, res: Response, next: NextFunction): any => {
+  const { method, path } = req
+  const { body, params } = req
 
   switch (true) {
     case method === 'POST' && path === '/post': {
       const { title, content } = body
-      if (!title || !content) badRequest(new MissingParamError('title or content'))
+      if (!title || !content) res.send(badRequest(new MissingParamError('title or content')))
       break
     }
     case method === 'POST' && path === '/comment': {
       const { content, postId } = body
-      if (!content || !postId) badRequest(new MissingParamError('content or postId'))
+      if (!content || !postId) res.send(badRequest(new MissingParamError('content or postId')))
       break
     }
     case method === 'GET' && path === '/posts': break
     case method === 'GET' && path.startsWith('/post/'): {
       const { id } = params
-      if (!id) return badRequest(new MissingParamError('id'))
+      if (!id) return res.send(badRequest(new MissingParamError('id')))
       break
     }
     case method === 'PATCH' && path.startsWith('/post/'): {
       const { title, content } = body
-      if (!title || !content) badRequest(new MissingParamError('title or content'))
+      if (!title || !content) res.send(badRequest(new MissingParamError('title or content')))
       break
     }
     case method === 'DELETE' && path.startsWith('/post/'): {
       const { id } = params
-      if (!id) badRequest(new MissingParamError('id'))
+      if (!id) res.send(badRequest(new MissingParamError('id')))
       break
     }
     case method === 'DELETE' && path.startsWith('/comment/'): {
       const { id } = params
-      if (!id) badRequest(new MissingParamError('id'))
+      if (!id) res.send(badRequest(new MissingParamError('id')))
       break
     }
   }
